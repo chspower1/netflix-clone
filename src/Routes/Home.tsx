54b31c34ea9c84997c0a933386ac6ddb9321a3be
chspower1 <chspower1@gmail.com>
2022-09-26 05:19:39 +0900

    react-query 복습

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,27 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "react-query";
-import { getMovies, getImage } from "../api";
-
-export interface Movie {
-    adult?: boolean;
-    backdrop_path?: string;
-    genre_ids?: number[];
-    id?: number;
-    original_language?: string;
-    original_title?: string;
-    overview?: string;
-    popularity?: number;
-    poster_path?: string;
-    release_date?: Date;
-    title?: string;
-    video?: boolean;
-    vote_average?: number;
-    vote_count?: number;
-}
+import { getMovies, getImage, Movies, Movie } from "../api";
 
 export default function Home() {
-    const { isLoading, data } = useQuery(["getMovies"], getMovies);
-    // const { results }: { results: Movie[] } = isLoading ? null : data;
-    console.log(data, isLoading);
-    return <div style={{ height: "300vh" }}>Home</div>;
+    const [movies, setMovies] = useState<Movie[]>();
+    const { isLoading } = useQuery<Movies>(["getMovies"], getMovies, {
+        onSuccess(data) {
+            setMovies(data.results);
+        },
+    });
+    if (isLoading) return <>Loading</>;
+    return (
+        <div>
+            <h1>Home</h1>
+            {movies?.map((movie) => (
+                <div>{movie.title}</div>
+            ))}
+        </div>
+    );
 }
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,34 @@
 const API_KEY = "1a301bdc4abdaca0b146fc986fe4436c";
 const BASE_PATH = "https://api.themoviedb.org/3";
 const BASE_IMG_PATH = "https://image.tmdb.org/t/p/original";
+
+export interface Movies {
+    data: MoviesDate;
+    page: 1;
+    results: Movie[];
+    total_pages: 4;
+    total_results: 62;
+}
+export interface MoviesDate {
+    maximun: string;
+    minimum: string;
+}
+export interface Movie {
+    adult?: boolean;
+    backdrop_path?: string;
+    genre_ids?: number[];
+    id?: number;
+    original_language?: string;
+    original_title?: string;
+    overview?: string;
+    popularity?: number;
+    poster_path?: string;
+    release_date?: Date;
+    title?: string;
+    video?: boolean;
+    vote_average?: number;
+    vote_count?: number;
+}
 export async function getMovies() {
     return await (
         await fetch(

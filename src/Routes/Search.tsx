e5572f20d771120417e71a14791ea6ce41cbc2be
chspower1 <chspower1@gmail.com>
2022-09-27 00:16:20 +0900

    완강, 프로젝트 시작

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function Search() {
+    const location = useLocation();
+    const keyword = new URLSearchParams(location.search).get("keyword");
+    console.log("keyword", keyword);
+    console.log("location", location);
     return <div>Search</div>;
 }
